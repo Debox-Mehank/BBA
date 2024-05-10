a06b786d762ee7a013884000d4752260ae57ad2f
Debox-Mehank <mehank@debox.co.in>
2024-05-10 13:30:51 +0530

    added hygraph endpoint in environment variables

--- apolloClient.js
+++ apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: `https://ap-south-1.cdn.hygraph.com/content/clozk8xcv36fw01uk9l1p3inv/master`,
+  uri: process.env.NEXT_API_ENDPOINT ?? "",
 
   cache: new InMemoryCache(),
 });
