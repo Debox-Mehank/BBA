1c603e11f791032baae37baa4fb8b3c93c256024
Debox-Mehank <mehank@debox.co.in>
2024-07-23 19:07:00 +0530

    reordered the floating buttons

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -27,6 +27,14 @@ const Layout = ({ children }) => {
       </Head>
       <Navbar />
       {children}
+      <a
+        target="_blank"
+        rel="noreffer"
+        href="https://ordersave.com/partnersite/j5DgkW8FF1Nd/menu"
+        className="bg-white/80 backdrop-blur-3xl fixed bottom-4 right-2 md:right-8 text-sm md:text-base z-[100] hover:bg-white duration-200 py-2 px-5 rounded-lg drop-shadow-2xl"
+      >
+        Order Online
+      </a>
       {nv ? (
         <button
           // target="_blank"
@@ -41,14 +49,6 @@ const Layout = ({ children }) => {
           Download our App
         </button>
       ) : null}
-      <a
-        target="_blank"
-        rel="noreffer"
-        href="https://ordersave.com/partnersite/j5DgkW8FF1Nd/menu"
-        className="bg-white/80 backdrop-blur-3xl fixed bottom-4 right-2 md:right-8 text-sm md:text-base z-[100] hover:bg-white duration-200 py-2 px-5 rounded-lg drop-shadow-2xl"
-      >
-        Order Online
-      </a>
       <Footer />
     </div>
   );
