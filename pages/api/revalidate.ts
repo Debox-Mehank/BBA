1668b12a9f70d97bf9cef9893fa0b504681a0a99
aditya-debox <aditya@debox.co.in>
2025-02-21 16:31:50 +0530

    added privacy policy

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -5,9 +5,9 @@ export default async function handler(
   res: NextApiResponse
 ) {
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
+  // if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
+  //   return res.status(401).json({ message: "Invalid token" });
+  // }
 
   try {
     await res.revalidate(req.query.path?.toString() ?? "");
